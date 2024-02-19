30cf835f53d27614d953c630dfdac504c7cd1313
Yan-Bakhmat <yan.bakhmat@gmail.com>
2024-02-19 20:18:22 +0100

    Create Tutorial.py

---
+++ Tutorial.py
@@ -0,0 +1,27 @@
+var = 123 # integer
+PI = 3.14 # float
+text = "Python" # string
+is_game = True # bool
+
+lst = list() # list
+tup = tuple() # tuple
+dct = dict() #dict
+
+print(var)
+
+var = var - 100
+
+print(var)
+
+text = text + '3'
+
+print(text)
+
+print(text[0])
+
+list_text = list(text)
+print(list_text)
+
+list_text[0] = 'F'
+
+print(list_text)
